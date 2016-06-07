726b4bc111380020a64fa358f924130cea9b8bfb
Kyle@Home <mk.bates@hotmail.com>
2016-06-07 09:32:03 -0600

    Reading from DB

--- app/build/generated/source/r/debug/com/comp259/contactlist/R.java
+++ app/build/generated/source/r/debug/com/comp259/contactlist/R.java
@@ -11,43 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_Add=0x7f09000c;
-        public static final int action_settings=0x7f09000b;
-        public static final int btnAdd=0x7f09000a;
-        public static final int btnDelete=0x7f090009;
-        public static final int btnSave=0x7f090000;
-        public static final int lblEmail=0x7f090008;
-        public static final int lblFName=0x7f090002;
-        public static final int lblLName=0x7f090005;
-        public static final int lblPNumber=0x7f090007;
-        public static final int txtEmail=0x7f090001;
-        public static final int txtFName=0x7f090003;
-        public static final int txtLName=0x7f090004;
-        public static final int txtPNumber=0x7f090006;
+        public static final int action_Add=0x7f08000c;
+        public static final int action_settings=0x7f08000b;
+        public static final int btnAdd=0x7f08000a;
+        public static final int btnDelete=0x7f080009;
+        public static final int btnSave=0x7f080000;
+        public static final int lblEmail=0x7f080008;
+        public static final int lblFName=0x7f080002;
+        public static final int lblLName=0x7f080005;
+        public static final int lblPNumber=0x7f080007;
+        public static final int txtEmail=0x7f080001;
+        public static final int txtFName=0x7f080003;
+        public static final int txtLName=0x7f080004;
+        public static final int txtPNumber=0x7f080006;
     }
     public static final class layout {
         public static final int activity_activity2=0x7f030000;
         public static final int activity_main=0x7f030001;
     }
     public static final class menu {
-        public static final int main=0x7f080000;
-        public static final int menu_activity2=0x7f080001;
-    }
-    public static final class raw {
-        public static final int contacts=0x7f040000;
+        public static final int main=0x7f070000;
+        public static final int menu_activity2=0x7f070001;
     }
     public static final class string {
-        public static final int action_settings=0x7f070000;
-        public static final int app_name=0x7f070001;
-        public static final int hello_world=0x7f070002;
-        public static final int title_activity_activity2=0x7f070003;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
+        public static final int title_activity_activity2=0x7f060003;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
@@ -58,9 +55,9 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f050001;
     }
 }
