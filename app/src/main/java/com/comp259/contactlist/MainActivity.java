c6e575c6c862a9502052d299055f4a78f6d570bf
Kyle@Home <mk.bates@hotmail.com>
2016-06-07 15:16:20 -0600

    Update kind of working, untested delete and add not working

--- app/src/main/java/com/comp259/contactlist/MainActivity.java
+++ app/src/main/java/com/comp259/contactlist/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -15,6 +16,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 
 
 public class MainActivity extends ListActivity{
@@ -22,6 +25,8 @@ public class MainActivity extends ListActivity{
 	DBAdapter db = new DBAdapter(this);
 	ListView listView;
 	Context myContext=null;
+    ArrayList<Contact> contactList = new ArrayList<Contact>();
+    ArrayAdapter<Contact> adapter;
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -59,7 +64,7 @@ public class MainActivity extends ListActivity{
 				DisplayContact(c);
 			} while (c.moveToNext());
 		}
-		db.close();
+		//db.close();
 	}
 
 	public void CopyDB(InputStream inputStream,
@@ -75,11 +80,13 @@ public class MainActivity extends ListActivity{
 	}
 	public void DisplayContact(Cursor c)
 	{
-		Toast.makeText(this,
-				"id: " + c.getString(0) + "\n" +
-						"Name: " + c.getString(1) + "\n" +
-						"Email:  " + c.getString(2),
-				Toast.LENGTH_LONG).show();
+		/*Toast.makeText(this, "id: " + c.getString(0) + "\n" + "Name: " + c.getString(1) + "\n" +
+				"Email:  " + c.getString(2), Toast.LENGTH_LONG).show();*/
+        Contact contact = new Contact(c.getString(1), c.getString(2), c.getString(3), c.getString(4));
+        contactList.add(contact);
+        listView = getListView();
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,contactList);
+        listView.setAdapter(adapter);
 	}
 
 
@@ -88,11 +95,8 @@ public class MainActivity extends ListActivity{
 
             Intent intent = new Intent("com.comp259.contactlist.Activity2");
             Bundle bundle = new Bundle();
-
 			bundle.putInt("Ref", -1);
-
 			intent.putExtras(bundle);
-
 			startActivityForResult(intent, 1);
 
 		}
@@ -100,18 +104,66 @@ public class MainActivity extends ListActivity{
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data ) {
 
+        Bundle updateBundle = data.getExtras();
+        int Id = updateBundle.getInt("Ref");
+        String newFname = updateBundle.getString("fName");
+        String newLname = updateBundle.getString("lName");
+        String newpNumber = updateBundle.getString("pNumber");
+        String newEmail = updateBundle.getString("email");
+        Contact c = new Contact(newFname, newLname, newpNumber, newEmail);
 
-	}
+        if (resultCode == -1) {
+
+            db.insertContact(newFname, newLname, newpNumber, newEmail);
+            updateDisplay();
+
+        } else if (resultCode == 1) {
+
+            db.updateContact(Id, newFname, newLname, newpNumber, newEmail);
+            updateDisplay();
+
+        } else if (resultCode == 2) {
+
+            db.deleteContact(Id);
+            updateDisplay();
+
+        }
+    }
 
 
 	@Override
 	protected void onListItemClick(ListView lv, View v, int pos, long id ){
 		super.onListItemClick(lv, v, pos, id);
 
+        Intent i = new Intent("com.comp259.contactlist.Activity2");
+        Bundle bundle = new Bundle();
+
+        bundle.putInt("Ref", pos);
+        bundle.putString("fName", contactList.get(pos).getFirstName());
+        bundle.putString("lName", contactList.get(pos).getLastName());
+        bundle.putString("pNumber", contactList.get(pos).getPhoneNumber());
+        bundle.putString("email", contactList.get(pos).getEmailAddress());
+
+        i.putExtras(bundle);
+
+        startActivityForResult(i, 1);
 
 	}
 
-	public void saveChanges() {
+	public void updateDisplay() {
+
+        db.open();
+        Cursor c = db.getAllContacts();
+        if (c.moveToFirst())
+        {
+            do {
+                DisplayContact(c);
+            } while (c.moveToNext());
+        }
+        db.close();
+
+        adapter=new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,contactList);
+        listView.setAdapter(adapter);
 
 	}
 }
